1f77ef94208f2cfc533cb64cc326b6e27eb8a803
Liang He <hyx520101@gmail.com>
2023-05-23 09:42:03 +0000

    version 1.9.0

--- R/plot_acet.R
+++ R/plot_acet.R
@@ -1,8 +1,8 @@
 plot_acet <- function(acet, boot = FALSE, heri = FALSE, xlab, ylab, main, col, legend = TRUE)
 {
-	if(!(class(acet) %in% c('AtCtEt_model', 'AtCtEp_mc_model','AtEtp_mc_model','AtCtEtp_mc_model')))
+	if(!(class(acet) %in% c('AtCtEt_model', 'AtDtEt_model', 'AtCtEp_mc_model','AtEtp_mc_model','AtCtEtp_mc_model')))
 	{
-		stop('The first argument must be an AtCtEt_model or AtCtEtp_mc_model object.')
+		stop('The first argument must be an AtCtEt_model, AtDtEt_model, or AtCtEtp_mc_model object.')
 	}
   
   if(missing(xlab))
@@ -28,7 +28,10 @@ plot_acet <- function(acet, boot = FALSE, heri = FALSE, xlab, ylab, main, col, l
 	{
 	  if(heri == FALSE)
 	  {  
-	    main_t <- "Variance curves of the A, C, and E components"
+	    if(is(acet,'AtDtEt_model')==FALSE)
+	    {main_t <- "Variance curves of the A, C, and E components"}else{
+	      main_t <- "Variance curves of the A, D, and E components"
+	    }
 	  }else{   
 	    main_t <- "Dynamic heritability"
 	  }
@@ -64,9 +67,18 @@ plot_acet <- function(acet, boot = FALSE, heri = FALSE, xlab, ylab, main, col, l
 		}else{
 			plot_AtCtEt_h(acet, boot, xlab=xlab_t, ylab=ylab_t, main=main_t, col=col)
 		}
-	}
+  }
+  
+  if(is(acet,'AtDtEt_model'))
+  {
+    if(heri == FALSE)
+    {
+      plot_AtDtEt(acet, boot, xlab=xlab_t, ylab=ylab_t, main=main_t, col=col, legend =legend)
+    }else{
+      plot_AtDtEt_h(acet, boot, xlab=xlab_t, ylab=ylab_t, main=main_t, col=col)
+    }
+  }
 	
-
 	# if(class(acet)=='AtCtEtp_mc_model')
   if(is(acet,'AtCtEtp_mc_model'))
 	{
